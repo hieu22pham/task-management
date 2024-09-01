781c88f70f327f07b7afad9ecfb0a6fa129f3712
hieu22pham <phammhieu2211@gmail.com>
2024-09-01 23:20:44 +0700

    update code

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -12,7 +12,12 @@ router.get("", async (req, res) => {
     find.status = req.query.status
   }
 
-  const tasks = await Task.find(find)
+  const sort = {}
+
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue
+  }
+  const tasks = await Task.find(find).sort(sort)
 
   res.json(tasks)
 })
