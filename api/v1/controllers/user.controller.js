a6008031210ade660af665f1465a817e9f472279
hieu22pham <phammhieu2211@gmail.com>
2024-09-07 13:05:39 +0700

    update code

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,5 +1,12 @@
+const md5 = require("md5")
 
+//[POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
+  req.body.password = md5(req.body.password)
+
+  console.log(req.body)
+
+  
 
   res.json({
     code: 200,
