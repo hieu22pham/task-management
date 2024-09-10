3a658c06afdabe49429358795ab406f44eb1c282
hieu22pham <phammhieu2211@gmail.com>
2024-09-10 22:36:46 +0700

    update code

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -130,6 +130,7 @@ module.exports.changeMulti = async (req, res) => {
 
 module.exports.create = async (req, res) => {
   try {
+    req.body.createdBy = req.user.id
     const task = new Task(req.body)
     const data = await task.save()
 
